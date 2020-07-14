4f4617bc061bcb583ae58dd08df39cd9ac84e7ad
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-14 07:07:09 +0200

    vendor: Port face unlock feature [2/4]
    Also add a flag to opt-out
    
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -116,3 +116,13 @@ include vendor/palladium/config/version.mk
 
 # Include extra packages
 include vendor/palladium/config/packages.mk
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
