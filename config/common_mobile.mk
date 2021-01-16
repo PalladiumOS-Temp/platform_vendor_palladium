4e693ca180d9f83b2cd03a2a6f6aab03af513a10
Han Wang <416810799@qq.com>
2021-01-16 15:02:35 +0100

    config: Remove persist.sys.dun.override again
     * It was removed in 2019, but somehow commit 8926990
       brought it back again.
    
    Change-Id: I973c03f7eed87c46973b42fd7bda04ed1ad27935

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,11 +1,6 @@
 # Inherit common mobile Palladium stuff
 $(call inherit-product, vendor/palladium/config/common.mk)
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# Thank you, please drive thru!
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.dun.override=0
-endif
-
 # Optional packages
 PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
