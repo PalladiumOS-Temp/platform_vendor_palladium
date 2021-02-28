c9966c2b4fb4e65edcb48acb201b552bc7d17944
zoldycc <sanjheevisv@gmail.com>
2021-02-28 17:43:52 +0530

    vendor: add few more props

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -13,4 +13,5 @@ ADDITIONAL_BUILD_PROPERTIES += \
   ro.palladium.buildtype=$(PALLADIUM_BUILDTYPE) \
   ro.palladium.fingerprint=$(PALLADIUM_FINGERPRINT) \
   ro.palladium.build.variant=$(PALLADIUM_BUILD_VARIANT) \
-  ro.palladium.device=$(PALLADIUM_BUILD) 
+  ro.palladium.device=$(PALLADIUM_BUILD) \
+  ro.palladium.build.version=$(PLATFORM_VERSION)
