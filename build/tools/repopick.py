AOSiP/platform_vendor_aosip

repopick: handle manifest commits
Change-Id: I3b58bda80e9061a013ec4e1849d6ec3dcd258abd
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

build/tools/repopick.py
@@ -317,6 +317,8 @@ if __name__ == '__main__':
         elif args.ignore_missing:
             print('WARNING: Skipping {0} since there is no project directory for: {1}\n'.format(item['id'], item['project']))
             continue
+        elif item['project'] == 'platform_manifest':
+            project_path = '.repo/manifests'
         else:
             sys.stderr.write('ERROR: For {0}, could not determine the project path for project {1}\n'.format(item['id'], item['project']))
             sys.exit(1)

PalladiumOS-Temp/platform_vendor_palladium

build/tools/repopick.py
@@ -383,6 +383,8 @@ if __name__ == '__main__':
         elif args.ignore_missing:
             print('WARNING: Skipping {0} since there is no project directory for: {1}\n'.format(item['id'], item['project']))
             continue
+        elif item['project'] == 'manifest':
+            project_path = '.repo/manifests'
         else:
             sys.stderr.write('ERROR: For {0}, could not determine the project path for project {1}\n'.format(item['id'], item['project']))
             sys.exit(1)