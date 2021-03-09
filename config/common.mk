bc8be2c2788918569fbc178a382502ebefadf542
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-03-10 00:41:51 +0530

    vendor: Guard faceunlock
    Signed-off-by: Lokesh773 <lokeshkasamneni773@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -115,6 +115,7 @@ DEVICE_PACKAGE_OVERLAYS += vendor/palladium/overlay/common
 include vendor/palladium/config/version.mk
 
 # Face Unlock
+ifeq ($(PALLADIUM_BUILDTYPE), OFFICIAL)
 TARGET_FACE_UNLOCK_SUPPORTED ?= true
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
@@ -123,6 +124,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 endif
 
 # Plugins
