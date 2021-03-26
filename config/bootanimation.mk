24b6bb4a60885b2bb87ad6be5065e0c6d736a952
sairpa <pranav.ajay2000@gmail.com>
2021-03-26 14:18:41 +0530

    vendor: update bootanimation

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -14,4 +14,17 @@
 
 #Bootanimation
 
-PRODUCT_COPY_FILES += vendor/palladium/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+LOCAL_PATH := vendor/palladium
+
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "Palladium-OS: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Palladium-OS: Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
