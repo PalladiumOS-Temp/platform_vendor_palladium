ZenX-OS/android_vendor_zenx

vendor: Introduce Lawnchair 11
- An Android 11 Quickstep Launcher that does not suck.

packages/apps/Lawnchair/lawnchair.mk
@@ -1,0 +1,8 @@
+# Lawnchair
+LAWN_ROOT := vendor/lineage/packages/apps/Lawnchair
+
+PRODUCT_PACKAGES += Lawnchair
+PRODUCT_PACKAGE_OVERLAYS += $(LAWN_ROOT)/overlay
+PRODUCT_COPY_FILES += \
+    $(LAWN_ROOT)/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lawnchair.xml \
+    $(LAWN_ROOT)/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml

PalladiumOS-Temp/platform_vendor_palladium

Signed-off-by: zoldycc <sanjheevisv@gmail.com>

packages/apps/Lawnchair/lawnchair.mk
@@ -1,0 +1,8 @@
+# Lawnchair
+LAWN_ROOT := vendor/palladium/packages/apps/Lawnchair
+
+PRODUCT_PACKAGES += Lawnchair
+PRODUCT_PACKAGE_OVERLAYS += $(LAWN_ROOT)/overlay
+PRODUCT_COPY_FILES += \
+    $(LAWN_ROOT)/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lawnchair.xml \
+    $(LAWN_ROOT)/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml